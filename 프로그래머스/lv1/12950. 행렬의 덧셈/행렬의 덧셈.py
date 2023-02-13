48e96e59808e7f8cdb0ee52cb7e59a88039cb480
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-14 00:58:34 +0900

    [level 1] Title: 행렬의 덧셈, Time: 25.08 ms, Memory: 20.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            arr1[i][j] += arr2[i][j]
+    return arr1
+
+# 빈 리스트에는 값을 넣을 수 없어서 .append()나 .insert()를 사용해야 한다.
+# 하지만 중첩리스트는 인덱스를 설정할 수 없어서 힘들었음
+    """
+    def solution(arr1, arr2):
+        answer = [[]]
+        for i in range(len(arr1)):
+            for j in range(len(arr1[i])):
+                answer[i][j] = arr1[i][j] + arr2[i][j]
+        return answer
+    """
