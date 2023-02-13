ed19ca6b0b2bcaeef8fd57b6c75441e51a5073a4
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-14 05:52:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,6 +1,19 @@
 def solution(n):
     a = []
-    while n != 0 :
+    while n != 0: # == while n :
         a.insert(0,n % 3)
         n = n // 3 
     return sum([a[i] * (3 ** i) for i in range(len(a))])
+
+
+# int(tmp,3) tmp 문자열을 3진수로
+"""
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    answer = int(tmp, 3)
+    return answer
+"""
