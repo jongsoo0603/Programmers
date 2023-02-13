328ecfabf60128a337fbbd953b277281fb14bcdc
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-14 02:39:21 +0900

    [level 1] Title: 행렬의 덧셈, Time: 25.02 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,11 +1,12 @@
 def solution(arr1, arr2):
+    
     for i in range(len(arr1)):
         for j in range(len(arr1[i])):
             arr1[i][j] += arr2[i][j]
     return arr1
 
 # 빈 리스트에는 값을 넣을 수 없어서 .append()나 .insert()를 사용해야 한다.
-# 하지만 중첩리스트는 인덱스를 설정할 수 없어서 힘들었음
+# 하지만 중첩리스트는 [[4],[6],[7],[9]] 이렇게 되기 때문에 힘들었음
     """
     def solution(arr1, arr2):
         answer = [[]]
