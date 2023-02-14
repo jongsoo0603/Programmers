b1214d1c5ec81aae1f58addf5f27f61d14ed9ecc
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-14 20:09:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,27 @@
+"""def solution(s):
+    answer = ""
+    for wrd in s.split(" "):
+        if wrd != '':
+            for i in range(len(wrd)):
+                if i % 2 ==0:
+                    answer += wrd[i].upper()
+                else:
+                    answer += wrd[i].lower()
+            answer += " "
+    return answer.strip()
+"""
+def solution(s):
+    answer = []
+    word=s.split(" ")
+    for i in word:
+        part=[]
+        for j in range(0,len(i)):
+            if j%2==0:
+                part.append(i[j].upper())
+            else:
+                part.append(i[j].lower())
+        part="".join(part)
+        answer.append(part)
+    answer=" ".join(answer)
+    print(answer)
+    return answer
