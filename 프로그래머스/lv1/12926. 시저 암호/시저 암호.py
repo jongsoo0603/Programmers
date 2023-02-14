e7a1f2d7dbeadf2bdcf0977ddb5b68c76b467f8b
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-14 23:08:00 +0900

    [level 1] Title: 시저 암호, Time: 2.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,3 +1,32 @@
 def solution(s, n):
     answer = ''
+    uplst = list(range(65,91))
+    lwlst = list(range(97,123))
+    for i in s:
+        if i.isupper():
+            if ord(i)+n > 90:
+                answer += chr(ord(i)+n - 26)
+            else:
+                answer += chr(ord(i)+n)
+        elif i.islower():
+            if ord(i)+n > 122:
+                answer += chr(ord(i)+n - 26)
+            else:
+                answer += chr(ord(i)+n)
+        else :
+            answer += " "
     return answer
+
+# 범위 계산해서 식 줄이기
+"""
+def solution(s, n):
+    answer = ''
+    for i in s:
+        if i.isupper():
+            answer += chr((ord(i) +n -ord('A')) % 26 + ord('A'))
+        elif i.islower():
+            answer += chr((ord(i) +n -ord('a')) % 26 + ord('a'))
+        else :
+            answer += " "
+    return answer
+"""
