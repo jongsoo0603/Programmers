c99c678ef393c8c454fd102d089b51701ffd43ff
jongsoo0603 <98942866+jongsoo0603@users.noreply.github.com>
2023-02-15 13:37:46 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,4 +1,7 @@
 def solution(number):
+    return len([k for i in range(len(number)) for j in range(i+1,len(number)) for k in range(j+1, len(number)) if number[i]+number[j]+number[k] == 0])
+"""
+def solution(number):
     answer = 0
     for i in range(len(number)):
         for j in range(i+1,len(number)):
@@ -6,3 +9,4 @@ def solution(number):
                 if number[i]+number[j]+number[k] == 0:
                     answer += 1
     return answer
+"""
